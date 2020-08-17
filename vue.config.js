d1e4d8dac3bf780139d1a1c12932568d176cf1d9
moses.zuo <moses.zuo@feisu.com>
2020-08-17 17:41:42 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  publicPath:process.env.NODE_ENV === 'production'? '/fs-design/': '/',
+  publicPath:process.env.NODE_ENV === 'production'? '/fs-design/dist/': '/',
   outputDir: "dist",
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === "production") {
